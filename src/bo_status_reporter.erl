0e3fe8be1466702bd297e20cc2eb70e097ec77ba
HernanRivasAcosta <hernanrivasacosta@gmail.com>
2016-08-24 12:20:20 -0300

    pr comments

--- src/bo_status_reporter.erl
+++ src/bo_status_reporter.erl
@@ -13,16 +13,18 @@ start() ->
 
 -spec tick() -> ok.
 tick() ->
-  Instructions = get_instructions(),
-  ok = erlcloud_ec2:configure(key(), secret()),
-  _ = erlcloud_s3:put_object(bucket(), filename(), Instructions),
+  Key = application:get_env(beam_olympics, s3_key),
+  Secret = application:get_env(beam_olympics, s3_secret),
+  Bucket = application:get_env(beam_olympics, s3_bucket),
+  ok = erlcloud_ec2:configure(Key, Secret),
+  _ = erlcloud_s3:put_object(Bucket, filename(), get_instructions()),
   ok.
 
 %%==============================================================================
 %% Utils
 %%==============================================================================
 get_instructions() ->
-  Filename = "lib/beam_olympics-private-1.0.0/priv/instructions.rtf",
+  Filename = "lib/beam_olympics-private-1.0.0/priv/" ++ filename(),
   {ok, Data} = file:read_file(Filename),
   replace(Data, [ {<<"{cookie}">>, atom_to_binary(erlang:get_cookie(), utf8)}
                 , {<<"{node}">>, atom_to_binary(erlang:node(), utf8)}]).
@@ -38,14 +40,5 @@ schedule_tick(Time) ->
   {ok, _TRef} = timer:apply_after(Time, ?MODULE, tick, []),
   ok.
 
-%%==============================================================================
-%% Constants
-%%==============================================================================
-key() ->
-  "AKIAIT5KCPYPSN3KEJHQ".
-secret() ->
-  "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".
-bucket() ->
-  "inaka-elixir-challenge".
 filename() ->
   "instructions.rtf".
