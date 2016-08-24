03f3a0e74fca18c1056fe9b4e182fb3868839cba
HernanRivasAcosta <hernanrivasacosta@gmail.com>
2016-08-24 16:37:12 -0300

    fixes

--- src/bo_status_reporter.erl
+++ src/bo_status_reporter.erl
@@ -1,7 +1,7 @@
 -module(bo_status_reporter).
 
 %% API
--export([start/0, tick/0]).
+-export([start/0]).
 
 %%==============================================================================
 %% API
@@ -9,13 +9,9 @@
 -spec start() -> ok.
 start() ->
   _ = erlcloud:start(),
-  ok = schedule_tick(1500).
-
--spec tick() -> ok.
-tick() ->
-  Key = application:get_env(beam_olympics, s3_key),
-  Secret = application:get_env(beam_olympics, s3_secret),
-  Bucket = application:get_env(beam_olympics, s3_bucket),
+  {ok, Key} = application:get_env(beam_olympics, s3_key),
+  {ok, Secret} = application:get_env(beam_olympics, s3_secret),
+  {ok, Bucket} = application:get_env(beam_olympics, s3_bucket),
   ok = erlcloud_ec2:configure(Key, Secret),
   _ = erlcloud_s3:put_object(Bucket, filename(), get_instructions()),
   ok.
@@ -24,7 +20,8 @@ tick() ->
 %% Utils
 %%==============================================================================
 get_instructions() ->
-  Filename = "lib/beam_olympics-private-1.0.0/priv/" ++ filename(),
+  PrivDir = priv_dir(),
+  Filename = filename:join(PrivDir, filename()),
   {ok, Data} = file:read_file(Filename),
   replace(Data, [ {<<"{cookie}">>, atom_to_binary(erlang:get_cookie(), utf8)}
                 , {<<"{node}">>, atom_to_binary(erlang:node(), utf8)}]).
@@ -36,9 +33,13 @@ replace(Bin, [H | T]) ->
 replace(Bin, {Pattern, Replacement}) ->
   binary:replace(Bin, Pattern, Replacement).
 
-schedule_tick(Time) ->
-  {ok, _TRef} = timer:apply_after(Time, ?MODULE, tick, []),
-  ok.
-
 filename() ->
   "instructions.rtf".
+
+priv_dir() ->
+  case code:priv_dir('beam_olympics-private') of
+    Path when is_list(Path) ->
+      Path;
+    _ ->
+      "lib/beam_olympics-private-1.0.0/priv/"
+  end.
