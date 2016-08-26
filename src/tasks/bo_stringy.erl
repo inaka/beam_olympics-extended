e91398cef55f9896ca8c3ed8c2bebfed9988d900
HernanRivasAcosta <hernanrivasacosta@gmail.com>
2016-08-26 11:27:31 -0300

    fixed the tests

--- src/tasks/bo_stringy.erl
+++ src/tasks/bo_stringy.erl
@@ -51,13 +51,13 @@ build_test(Bin) ->
 build_cases() ->
   [ "Is this really that complicated? It should be trivial... Unless someone "
     "went in and added a number, say 1234 in the middle of the phrase."
-  , "Once upon a time, there was a boy. He lived in a village that no longer "
+  , " Once upon a time, there was a boy. He lived in a village that no longer "
     "exists, in a house that no longer exists, on the edge of a field that no "
     "longer exists"
   , "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
     "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
-    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ""
-    commodo consequat"
+    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
+    "commodo consequat"
   , "Programming today is a race between software engineers striving to build "
     "bigger and better idiot-proof programs, and the universe trying to build "
     "bigger and better idiots. So far, the universe is winning."
@@ -79,28 +79,28 @@ build_cases() ->
     " upon execution."
   , "Should array indices start at 0 or 1? My compromise of 0.5 was rejected "
     "without, I thought, proper consideration."
-  , "It would be really mean to test how you do the alphabet wouldn't it? too"
+  , "It would be really mean to test how you do the alphabet wouldn't it? too "
     "bad, here it is: a b c d e f g h i j (would a parenthesis complicate "
-    "things?) k l m n o p q r s t u (and what about 2?) v w x y z."
+    "things even more?) k l m n o p q r s t u (and what this: (())) v w x y z."
   ].
 
 solve(Bin) ->
   Words = [binary_to_list(B) || B <- binary:split(Bin, <<" ">>, [global])],
-  {_, Result} = lists:foldl(fun(W, {true, Acc}) ->
-                                case word(W) of
-                                  true  -> {false, [up(W)| Acc]};
-                                  false -> {true, [W | Acc]}
-                                end;
-                               (W, {false, Acc}) ->
-                                case word(W) of
-                                  true  -> {true,  [W | Acc]};
-                                  false -> {false, [W | Acc]}
+  {_, Result} = lists:foldl(fun(W, {LastUpper, Acc}) ->
+                                case {LastUpper, word(W)} of
+                                  {true, true}   -> {false, [up(W)| Acc]};
+                                  {false, false} -> {false, [W | Acc]};
+                                  _Other         -> {true, [W | Acc]}
                                 end
                             end, {true, []}, Words),
   list_to_binary(string:join(lists:reverse(Result), " ")).
 
+word([$( | Rest]) ->
+  word(Rest);
 word([Char | _]) ->
-  (Char >= $a andalso Char =< $z) orelse (Char >= $A andalso Char =< $Z).
+  (Char >= $a andalso Char =< $z) orelse (Char >= $A andalso Char =< $Z);
+word(_) ->
+  false.
 
 up([Char | Rest]) when Char >= $a, Char =< $z ->
   [Char + $A - $a | Rest];
